7f14b2a5c7183538fa0d499dc9382f41b0974ffb
wanga <1911847374@qq.com>
2020-12-10 17:13:48 +0800

    daily change

--- dataStr_Alg/sort/bubbleSort.c
+++ dataStr_Alg/sort/bubbleSort.c
@@ -1,3 +1,7 @@
+/*
+	描述：冒泡排序
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -5,11 +9,13 @@ int main()
 {
 	int a[]={21,25,49,25,16,8};
 	int i,j,temp;
-	for(i=0;i<6;i++)
+	
+	for(i=0;i<6;i++)				//打印原数组元素
 		printf("%d ",a[i]);
 	printf("\n");
-	for(j=0;j<5;j++)
-		for(i=0;i<5-j;i++)
+	
+	for(j=0;j<5;j++)				//第一层循环约定需要确定几个位置
+		for(i=0;i<5-j;i++)			//第二层循环约定确定哪个位置
 		{
 			if(a[i]>a[i+1])
 			{
@@ -18,7 +24,9 @@ int main()
 				a[i+1]=temp;
 			}
 		}
-	for(i=0;i<6;i++)
+	
+	for(i=0;i<6;i++)				//打印排序后数组元素
 		printf("%d ",a[i]);
 	printf("\n");
+
 }
