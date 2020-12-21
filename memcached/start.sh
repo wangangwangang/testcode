157437e11bba143e61768b2002ca9a8897757530
wanga <1911847374@qq.com>
2020-12-21 15:47:30 +0800

    daily change

--- memcached/start.sh
+++ memcached/start.sh
@@ -11,12 +11,12 @@
 
 #!/bin/sh
 
-memcached -d -m 128 -u wanga -p 11111 -P ./ma.pid
+memcached -d -m 128 -u wanga -p 11111 -P /work/test1/wa/ma.pid
 if [ $? -eq 0 ];then
 	echo "start ma succeed,port:[11111]"
 fi
 
-memcached -d -m 128 -u wanga -p 11112 -P ./ba.pid
+memcached -d -m 128 -u wanga -p 11112 -P /work/test1/wa/ba.pid
 if [ $? -eq 0 ];then
 	echo "start ba succeed,port[11112]"
 fi
--- memcached/stop.sh
+++ memcached/stop.sh
@@ -4,19 +4,32 @@
 #   文件名称：stop.sh
 #   创 建 者：Ang Wang
 #   创建日期：2020年12月17日
-#   描    述：如果-P参数启动memcached时无法创建.pid文件时，可以用这种办法结束进程
+#   描    述：
 #
 #================================================================
 
 
 #!/bin/sh
 
+#无.pid文件时停掉进程的方法
+if false;then
 kill -9 `lsof -i :11111 | grep "memcached" | awk '{print $2}'`
 if [ $? -eq 0 ];then
 	echo "stop ma succeed,port[11111]"
 fi
 
 kill -9 `lsof -i :11112 | grep "memcached" | awk '{print $2}'`
+if [ $? -eq 0 ];then
+	echo "stop ba succeed,port[11112]"
+fi
+fi
+
+kill -9 `cat /work/test1/wa/ma.pid`
+if [ $? -eq 0 ];then
+	echo "stop ma succeed,port[11111]"
+fi
+
+kill -9 `cat /work/test1/wa/ba.pid`
 if [ $? -eq 0 ];then
 	echo "stop ba succeed,port[11112]"
 fi
