00d84e33780470168c5c7da3384ae15c258d3d9e
wanga <1911847374@qq.com>
2020-12-23 00:36:16 +0800

    daily change

--- memcached/start.sh
+++ memcached/start.sh
@@ -11,12 +11,12 @@
 
 #!/bin/sh
 
-memcached -d -m 128 -u wanga -p 11111 -P /work/test1/wa/ma.pid
+memcached -d -m 128 -u wanga -p 11111 -P `pwd`/ma.pid		#-P后必须是绝对路径，相对路径不可以
 if [ $? -eq 0 ];then
 	echo "start ma succeed,port:[11111]"
 fi
 
-memcached -d -m 128 -u wanga -p 11112 -P /work/test1/wa/ba.pid
+memcached -d -m 128 -u wanga -p 11112 -P `pwd`/ba.pid
 if [ $? -eq 0 ];then
-	echo "start ba succeed,port[11112]"
+	echo "start ba succeed,port:[11112]"
 fi
--- memcached/stop.sh
+++ memcached/stop.sh
@@ -12,7 +12,7 @@
 #!/bin/sh
 
 #无.pid文件时停掉进程的方法
-if false;then
+#if false;then
 kill -9 `lsof -i :11111 | grep "memcached" | awk '{print $2}'`
 if [ $? -eq 0 ];then
 	echo "stop ma succeed,port[11111]"
@@ -22,8 +22,10 @@ kill -9 `lsof -i :11112 | grep "memcached" | awk '{print $2}'`
 if [ $? -eq 0 ];then
 	echo "stop ba succeed,port[11112]"
 fi
-fi
+#fi
 
+#有.pid文件时停掉进程的方法
+if false;then
 kill -9 `cat /work/test1/wa/ma.pid`
 if [ $? -eq 0 ];then
 	echo "stop ma succeed,port[11111]"
@@ -32,4 +34,5 @@ fi
 kill -9 `cat /work/test1/wa/ba.pid`
 if [ $? -eq 0 ];then
 	echo "stop ba succeed,port[11112]"
+fi
 fi
